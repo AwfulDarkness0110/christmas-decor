4110efd67fe9fa33efbe3bd13637efc92b1e14f0
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2022-07-30 17:43:33 +0300

    Add hamburger menu to separate file

---
+++ docs/js/hamburger.js
@@ -0,0 +1,22 @@
+'use strict';
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navbar-container');
+const socialMenu = document.querySelector('.social-icons-container');
+const body = document.querySelector('body');
+function hamburgerMenu() {
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    socialMenu.classList.toggle('active');
+    body.classList.toggle('active');
+  });
+  document.querySelectorAll('.nav-link').forEach((l) =>
+    l.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      navMenu.classList.remove('active');
+      socialMenu.classList.remove('active');
+      body.classList.remove('active');
+    })
+  );
+}
+export default hamburgerMenu;
--- docs/js/script.js
+++ docs/js/script.js
@@ -1,26 +1,10 @@
 'use strict';
 import tabs from '../js/tabs.js';
 import generateShop from '../js/generate-shop.js';
-import accordion from '../js/accordion';
+import accordion from '../js/accordion.js';
+import hamburgerMenu from '../js/hamburger.js';
 // --------Hamburger menu------------
-const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.navbar-container');
-const socialMenu = document.querySelector('.social-icons-container');
-const body = document.querySelector('body');
-hamburger.addEventListener('click', () => {
-  hamburger.classList.toggle('active');
-  navMenu.classList.toggle('active');
-  socialMenu.classList.toggle('active');
-  body.classList.toggle('active');
-});
-document.querySelectorAll('.nav-link').forEach((l) =>
-  l.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-    socialMenu.classList.remove('active');
-    body.classList.remove('active');
-  })
-);
+hamburgerMenu();
 // ------------Turn on tabs---------
 tabs();
 // ------------Generate all product cards on website------

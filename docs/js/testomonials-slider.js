1d5dad58543f2bb65f7888c98e33528b2a59338d
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2022-08-01 00:06:28 +0300

    Add Testimonials slider

---
+++ docs/js/testomonials-slider.js
@@ -0,0 +1,16 @@
+const testimonialsContainers = [...document.querySelectorAll('.testimonials')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+testimonialsContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener('click', () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener('click', () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
